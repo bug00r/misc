99c4a45637dd6dc5bba43aa33c931ddd924f9152
bug00r <bachus@gmx.net>
2019-03-25 16:03:31 +0100

    start up lexicon

--- _javascript/projects/dsa_portal/resources/js/lexicon_vue.js
+++ _javascript/projects/dsa_portal/resources/js/lexicon_vue.js
@@ -1,6 +1,6 @@
 dsa.tools.lexicon.vue = {
   el: '#dsa_portal_lexicon',
   data: {
-    headline: dsa.resources.text.labels["dsa.portal.tool.lexicon.name"]
+    labels: dsa.resources.text.labels
   }
 }
