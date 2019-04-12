52caa4037dc7bfa349b42a8c419bc45d9ed0c776
bug00r <bachus@gmx.net>
2019-04-12 15:03:35 +0200

    add, remove hero and edit name

--- _javascript/projects/dsa_portal/resources/js/bin/hero.js
+++ _javascript/projects/dsa_portal/resources/js/bin/hero.js
@@ -1,14 +1,19 @@
+dsa.services = dsa.services|| {}
 
-dsa.services = {}
+dsa.services.heroService = new(class HeroService {
 
-services.heroService = new(class HeroService {
     constructor () {
         //TODO add localstorage for heros
         this._heros = new DOMParser().parseFromString("<heros></heros>", "application/xml");
     }
-    addHero (name) {
-        console.log(`add hero ${name}`);
+    addHero () {
+        const hero_el = dsa.resources.xml.basehero.getElementsByTagName("hero")[0].cloneNode(true);
+        hero_el.setAttribute("name", "");
+        hero_el.setAttribute('id', Date.now());
+        this._heros.adoptNode(hero_el);
+        this._heros.getElementsByTagName("heros")[0].appendChild(hero_el);
+        return hero_el;
     }
 })();
 
-dsa.services = services;
+
--- _javascript/projects/dsa_portal/resources/js/hgen_vue.js
+++ _javascript/projects/dsa_portal/resources/js/hgen_vue.js
@@ -2,9 +2,10 @@
 dsa.tools.hgen.vue = {
   el: '#dsa_portal_hgen',
   data: {
+    heros: dsa.services.heroService._heros.getElementsByTagName('heros')[0],
     labels: dsa.resources.text.labels,
-    heros: [],
-    currenthero: -1
+    currenthero: null,
+    currentheroid: -1,
   },
   beforeMount: function() {
 
@@ -13,6 +14,29 @@ dsa.tools.hgen.vue = {
      
   },
   methods: {
-    
+    selectHero : function(event) {
+      let id = $(event.target).val();
+      if (id > 0) {
+        this.currenthero = this.heros.querySelector(`hero[id='${id}']`);
+      }
+    },
+    addNewHero : function() {
+      let new_hero = dsa.services.heroService.addHero();
+      this.currenthero = new_hero;
+      this.currentheroid = new_hero.getAttribute('id');
+    },
+    removeHero : function() {
+      const deleted_hero = this.heros.removeChild(this.currenthero);
+      this.currenthero = null;
+      this.currentheroid = -1;
+    },
+    refreshHerolistEntry: function() {
+      this.currentheroid = -1;
+      this.currentheroid = this.currenthero.getAttribute('id');
+    }, 
+    getHeroNameByIndex: function(index) {
+      let name = this.heros.children[index].getAttribute('name');
+      return name ? name : this.labels['dsa.portal.tool.hgen.noname'];
+    }
   }
 }
